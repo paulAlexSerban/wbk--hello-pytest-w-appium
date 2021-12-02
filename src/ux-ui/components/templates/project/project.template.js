0c6da1ce753e7a8d216240b134676944395ae791
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-02 21:36:08 +0200

    el-vir_26 | updated project template js and scss

--- src/ux-ui/components/templates/project/project.template.js
+++ src/ux-ui/components/templates/project/project.template.js
@@ -1,21 +1,34 @@
-import './project.template.scss';
 import Navigation from '../../molecules/navigation/navigation.molecule';
 import Header from '../../organisms/header/header.organism';
 import Footer from '../../organisms/footer/footer.organism';
 
 export default class ProjectTemplate {
-  constructor(pageParams) {
-    this.pageParams = pageParams;
+  constructor(parentPageParams) {
     this.templateParams = {
-      templateName: 'generic',
+      parentPageParams,
     };
-    this.NAVIGATION = new Navigation(this.pageParams, this.templateParams);
-    this.HEADER = new Header(this.pageParams, this.templateParams);
-    this.FOOTER = new Footer(this.pageParams, this.templateParams);
+
     this.init();
   }
 
+  setupMolecules() {
+    document.querySelectorAll('[data-js-component="navigation"]').forEach((molecule) => {
+      this.NAVIGATION = new Navigation(molecule, this.pageParams, this.templateParams);
+    });
+  }
+
+  setupOrganisms() {
+    document.querySelectorAll('[data-js-component="header"').forEach((organism) => {
+      this.HEADER = new Header(organism, this.pageParams, this.templateParams);
+    });
+
+    document.querySelectorAll('[data-js-component="footer"]').forEach((organism) => {
+      this.FOOTER = new Footer(organism, this.pageParams, this.templateParams);
+    });
+  }
+
   init() {
-    console.log('project template loaded', this.pageParams);
+    this.setupMolecules();
+    this.setupOrganisms();
   }
 }
