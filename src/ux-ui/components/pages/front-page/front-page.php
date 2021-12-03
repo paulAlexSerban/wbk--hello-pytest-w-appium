fc1d5a97e46fe7aee71c9128b7112c3e829fdd5f
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-03 16:26:12 +0200

    el-vir_32 | updated front-page structure

--- src/ux-ui/components/pages/front-page/front-page.php
+++ src/ux-ui/components/pages/front-page/front-page.php
@@ -3,29 +3,26 @@ themeFiles('/front-page/', true);?>
 <main class="main__base" data-js-component="main" data-js-page="front-page">
   <div class="carousel__base uk-position-relative uk-visible-toggle" data-js-component="carousel" tabindex="-1" uk-slideshow="animation: fade; ratio: false;">
     <ul class="carousel__list uk-slideshow-items">
-
-<?php
-    $projects = new WP_Query(array(
-      'post_type' => 'work_project'
-    ));
-  ?>
-  <?php while($projects->have_posts()) {
-    $projects->the_post(); ?>
-    <li class="carousel__item">
-      <a class="carousel__slide uk-position-cove uk-animation-reverse uk-transform-origin-center-left"
-        href="<?php the_permalink(); ?>"
-        target="_blank">
-        <?php the_post_thumbnail('full', 
-                                  array( 'class' => 'carousel__image',
-                                          'uk-img' => 'target: !* -*, !* +*',
-                                          'uk-cover' => '',
-                                          'uk-img' => ''
-                                        )); ?>
-      </a>
-    </li>
-
-  <?php } ?>
-
+      <?php
+          $projects = new WP_Query(array(
+            'post_type' => 'work_project'
+          ));
+        ?>
+        <?php while($projects->have_posts()) {
+          $projects->the_post(); ?>
+          <li class="carousel__item">
+            <a class="carousel__slide uk-position-cove uk-animation-reverse uk-transform-origin-center-left"
+              href="<?php the_permalink(); ?>"
+              target="_blank">
+              <?php the_post_thumbnail('full', 
+                                        array( 'class' => 'carousel__image',
+                                                'uk-img' => 'target: !* -*, !* +*',
+                                                'uk-cover' => '',
+                                                'uk-img' => ''
+                                              )); ?>
+            </a>
+          </li>
+        <?php } ?>
     </ul>
     <a class="carousel__control uk-position-center-left uk-position-small uk-hidden-hover" href="#"
       uk-slidenav-previous uk-slideshow-item="previous"></a>
