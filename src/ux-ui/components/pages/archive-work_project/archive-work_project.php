4bbe0b8574fb70f254eab193aee25806f19243bd
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-04 02:08:08 +0200

    el-vir_37 | added card molecule

--- src/ux-ui/components/pages/archive-work_project/archive-work_project.php
+++ src/ux-ui/components/pages/archive-work_project/archive-work_project.php
@@ -1,15 +1,31 @@
-<?php get_header();
-themeFiles('/archive-work_project/', true);?>
+<?php
+// Page template logic
+$projects = new WP_Query(array(
+  'post_type' => 'work_project'
+));
+?>
 
-  <?php
-    $projects = new WP_Query(array(
-      'post_type' => 'work_project'
-    ));
-  ?>
-  <?php while($projects->have_posts()) {
-    $projects->the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <div><?php the_post_thumbnail(); ?></div>
-  <?php } ?>
+<?php get_header();
+      themeFiles('/archive-work_project/', true);?>
+<main class="main__base">
+  <div class="uk-child-width-expand@s uk-text-center" uk-grid>
+    <?php 
+    while($projects->have_posts()) {
+      $projects->the_post(); ?>
+        <div class="card__base">
+          <?php 
+            the_post_thumbnail(
+              'large',
+              array(
+                'class' => 'card__image',
+                'uk-img' => ''
+              )
+            ); 
+          ?>
+        </div>
+    <?php } ?>
+  </div>
+</main>
+
 
 <?php get_footer(); ?>
