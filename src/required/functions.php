fa8dc19e690af6d17f2187f8493388ed46ffb142
paulAlexSerban <paul.alex.serban@gmail.com>
2022-01-22 16:32:39 +0200

    updated uikit refrences and loading scripts

--- src/required/functions.php
+++ src/required/functions.php
@@ -11,13 +11,16 @@ function themeFiles($args, $hasUiKit) {
         wp_enqueue_script("{$key}", get_theme_file_uri('/assets/'.$value), NULL, '1.0', true);
       }
     }
-
-    if($hasUiKit) {
-      wp_enqueue_style('uikit', '//cdn.jsdelivr.net/npm/uikit@3.8.0/dist/css/uikit.min.css');
-      wp_enqueue_script('uikit-min', get_theme_file_uri('/assets/scripts/uikit.script.js'), NULL, '1.0', true);
-    }
   }
 }
+
+function load_vendor_scripts () {
+  wp_enqueue_style('uikit-css', '//cdn.jsdelivr.net/npm/uikit@3.10.1/dist/css/uikit.min.css');
+  wp_enqueue_script('uikit-min', '//cdn.jsdelivr.net/npm/uikit@3.10.1/dist/js/uikit.min.js', NULL, '1.0', true);
+  wp_enqueue_script('uikit-icons', '//cdn.jsdelivr.net/npm/uikit@3.10.1/dist/js/uikit-icons.min.js', NULL, '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'load_vendor_scripts');
 
 // function el_virtuoso() {
   // wp_enqueue_script('uikit', get_theme_file_uri('/assets/scripts/uikit.script.js'), '1.0', true);
